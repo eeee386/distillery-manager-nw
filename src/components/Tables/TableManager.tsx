256901147cdb44a6fdc64027cb7854057cf4ddcc
Attila Sedon <attilasedon@pop-os.localdomain>
2021-01-23 10:52:58 +0100

    fix auto fill

--- src/components/Tables/TableManager.tsx
+++ src/components/Tables/TableManager.tsx
@@ -8,17 +8,29 @@ import TableForm from './TableForm/TableForm';
 import {ConnectedComponentProps} from '../../models/ConnectTypes/ConnectTypes';
 import {Distillation} from '../../models/Distillation/Distillation';
 import {ActionFactory, Action} from '../../ReduxStoreHandlers/actionFactory';
+import {initialize} from 'redux-form';
 
 interface TableManagerProps {
     table: Distillation[];
 }
 
+const form = 'addNew';
+
 class TableManager extends React.Component<ConnectedComponentProps & TableManagerProps> {
+
+    componentDidUpdate(prevProps: Readonly<ConnectedComponentProps & TableManagerProps>, prevState: Readonly<{}>) {
+        if(prevProps.table !== this.props.table){
+            this.props.resetAddNewForm();
+        }
+    }
+
     render() {
         const {table, addNewDistillation, updateDistillation, deleteDistillation} = this.props;
         return (
             <div className={'tableWrapper'}>
-                <TableForm onSubmit={addNewDistillation} form={'addNew'}/>
+                <TableForm onSubmit={(data) => {
+                    addNewDistillation(data);
+                }} form={form}/>
                 {isEmpty(table) ? "Nincsenek főzetések" :
                     <TableList table={table} updateDistillation={updateDistillation}
                                deleteDistillation={deleteDistillation}/>}
@@ -31,6 +43,7 @@ const matchDispatchToProps = (dispatch: React.Dispatch<Action>) => ({
     addNewDistillation: (newDist: Distillation) => dispatch(ActionFactory(tableSagaTypes.ADD_NEW, newDist)),
     updateDistillation: (updatedDist: Distillation) => dispatch(ActionFactory(tableSagaTypes.UPDATE_ONE, updatedDist)),
     deleteDistillation: (deletedDist: Distillation) => dispatch(ActionFactory(tableSagaTypes.DELETE_ONE, deletedDist)),
+    resetAddNewForm: () => dispatch(initialize(form, {}) as Action)
 });
 
 export default connect(null, matchDispatchToProps)(TableManager);
